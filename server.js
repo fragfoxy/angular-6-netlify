26dcd3809c3d80f65505fb80db08b6391134f9a9
fragfoxy <fragfoxy@gmail.com>
2018-06-13 15:12:41 +0300

    nahyi idi

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static('./dist/Macmapp'));
+app.use(express.static('./dist/macapp'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/Macapp/index.html'));
+  res.sendFile(path.join('./dist/macapp/index.html'));
 });
 app.listen(process.env.PORT || 8080);
