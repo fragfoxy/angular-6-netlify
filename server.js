ef4591f94221db0c8a0e4e5b835f88eec109deb7
fragfoxy <fragfoxy@gmail.com>
2018-06-14 19:28:34 +0300

    last

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-app.use(express.static('./dist/macapp'));
-app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/macapp/index.html'));
-});
-app.listen(process.env.PORT || 8080);
